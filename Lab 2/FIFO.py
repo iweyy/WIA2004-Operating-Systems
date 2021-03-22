71f51df7162aef069193a688b9044b80ff86d9e8
Muhamad Ikhwan <wic190025@siswa.um.edu.my>
2021-03-22 22:58:32 +0800

    lab 2 change

---
+++ Lab 2/FIFO.py
@@ -0,0 +1,22 @@
+pageFrame = list(range(2))
+job = ['a','b','a','c','a','b','d','b','a','c','d']
+
+for i in range(len(pageFrame)):
+    pageFrame[i]=-1
+hit=0
+fault=0
+j=0
+for i in range(len(job)):
+    check=False
+    for k in range(len(pageFrame)):
+        if (pageFrame[k]==job[i]):
+            check=True
+            hit+=1
+    if (check==False):
+        pageFrame[j]=job[i]
+        j+=1
+        if(j>=len(pageFrame)):
+            j=0
+        fault+=1
+rat=hit/len(job)
+print("Hit: ", hit ," Fault: ", fault , "Hit ratio: ", rat)
