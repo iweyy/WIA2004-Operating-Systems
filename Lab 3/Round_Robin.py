3955f3c92b725ef0742f5a3b21c6a058d6a3e4d7
Muhamad Ikhwan <wic190025@siswa.um.edu.my>
2021-04-06 22:17:34 +0800

    round robin

--- Lab 3/Round_Robin.py
+++ Lab 3/Round_Robin.py
@@ -1,8 +1,47 @@
-noOfJob=int(input("Enter number of job: "))
-quantumTime=int(input("Enter Quantum Time: "))
-arrivalTime=list(range(noOfJob))
-cpuCycle=list(range(noOfJob))
-for i in range(noOfJob):
-    print("Enter Arrival Time and CPU Cycle for Job ",i+1)
-    arrivalTime[i]=int(input("Arrival Time: "))
-    cpuCycle[i]=int(input("CPU Cycle: "))
+arrivalTime=[0,1,2,3]
+burstTime=[8,4,9,5]
+temp=[]
+n=len(burstTime)
+qt=4
+jobDone=[]
+wt=[]
+tt=[]
+count=time=remain=flag=waitingTime=turnAroundTime=0
+remain=n
+seq=" "
+que=" "
+for i in burstTime:
+    temp.append(i)
+
+while remain!=0:
+    if temp[count]<=qt and temp[count]>0:
+        time+=temp[count]
+        temp[count]=0
+        flag=1
+        seq+="->P"+str((count+1))
+    elif temp[count]>0:
+        temp[count]-=qt
+        time+=qt
+        seq+="->P"+str((count+1))
+    if temp[count]==0 and flag==1:
+        remain-=1
+        jobDone.append(count+1)
+        wt.append(time-arrivalTime[count])
+        tt.append(time-arrivalTime[count]-burstTime[count])
+        waitingTime+=time-arrivalTime[count]-burstTime[count]
+        turnAroundTime+=time-arrivalTime[count]
+        flag=0
+    if count==n-1:
+        count=0
+    elif arrivalTime[count+1]<=time:
+        count+=1     
+    else:
+        count=0
+    print(seq)
+
+print("Job \t waiting time \t turn around time")
+for i in range(n):
+    print("P",jobDone[i],"\t",wt[i],"\t \t ",tt[i])
+print("sequence ",seq)
+print("average waiting time ",(waitingTime*1.0/n))
+print("average turn around time ",(turnAroundTime*1.0/n))
