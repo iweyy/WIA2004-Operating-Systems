0441c94094d7238d78043be23a035e124a94ae86
Muhamad Ikhwan <wic190025@siswa.um.edu.my>
2021-04-07 12:36:52 +0800

    update rr

--- Lab 3/Round_Robin.py
+++ Lab 3/Round_Robin.py
@@ -1,38 +1,39 @@
-arrivalTime=[0,1,2,3]
-burstTime=[8,4,9,5]
-temp=[]
-n=len(burstTime)
+arrivalTime=[0,0,0,0]
+cpuCycle=[8,4,9,5]
+temp_cc=[]
+n=len(cpuCycle)
 qt=4
 jobDone=[]
 wt=[]
 tt=[]
-count=time=remain=flag=waitingTime=turnAroundTime=0
-remain=n
+count=time=flag=waitingTime=turnAroundTime=0
+remain=n    # indicate when can exit while loop
 seq=" "
-que=" "
-for i in burstTime:
-    temp.append(i)
+for i in cpuCycle:
+    temp_cc.append(i)   # update cpu cycle
 
 while remain!=0:
-    if temp[count]<=qt and temp[count]>0:
-        time+=temp[count]
-        temp[count]=0
-        flag=1
+    if temp_cc[count]<=qt and temp_cc[count]>0:
+        time+=temp_cc[count]
+        temp_cc[count]=0
+        flag=1  # indicate the Job is done
         seq+="->P"+str((count+1))
-    elif temp[count]>0:
-        temp[count]-=qt
+        print("Job P"+str(count+1)," is coming in and done")
+    elif temp_cc[count]>0:
+        temp_cc[count]-=qt
         time+=qt
         seq+="->P"+str((count+1))
-    if temp[count]==0 and flag==1:
+        print("Job P"+str(count+1)," is coming in")
+    if temp_cc[count]==0 and flag==1:
         remain-=1
         jobDone.append(count+1)
         wt.append(time-arrivalTime[count])
-        tt.append(time-arrivalTime[count]-burstTime[count])
-        waitingTime+=time-arrivalTime[count]-burstTime[count]
+        tt.append(time-arrivalTime[count]-cpuCycle[count])
+        waitingTime+=time-arrivalTime[count]-cpuCycle[count]
         turnAroundTime+=time-arrivalTime[count]
         flag=0
     if count==n-1:
-        count=0
+        count=0 # check for another loop
     elif arrivalTime[count+1]<=time:
         count+=1     
     else:
@@ -42,6 +43,7 @@ while remain!=0:
 print("Job \t waiting time \t turn around time")
 for i in range(n):
     print("P",jobDone[i],"\t",wt[i],"\t \t ",tt[i])
-print("sequence ",seq)
+
 print("average waiting time ",(waitingTime*1.0/n))
 print("average turn around time ",(turnAroundTime*1.0/n))
+print("sequence ",seq)
